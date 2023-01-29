f9242e8bd78c3ba165748bfc4b71a46e1df72fde
vasujhawar2001 <vasujhawar2001@gmail.com>
2023-01-30 04:57:09 +0530

    console errors resolved

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -41,7 +41,7 @@ const NavBar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = "Vasu Jhawar" ||`${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
